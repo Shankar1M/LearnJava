dfd3f071d010c695630ed0e0b3749c9cadbc100d
Shankar1M <shankarmurthy9997@gmail.com>
2025-03-05 12:52:55 +0530

    mutable strings programs started

--- src/strings/Demo8.java
+++ src/strings/Demo8.java
@@ -1,5 +1,15 @@
+// mutable strings, in this strings we can StringBuffer or StringBuilder to create string objects
+// to add strings to the existing string we use append() in mutable strings
 package strings;
 
 public class Demo8 {
+	
+	public static void main(String[] args) {
+		StringBuffer s1 = new StringBuffer("Kod");
+		System.out.println(s1);
+		s1.append("Nest");
+		System.out.println(s1);
+	}
+	
 
 }
