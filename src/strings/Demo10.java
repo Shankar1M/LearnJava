ba7c1d623f1fbb6f13e511e7c9ba07853e0323b3
Shankar1M <shankarmurthy9997@gmail.com>
2025-03-05 13:06:48 +0530

    checking the capacity of StringBuffer and StringBuilder program added

---
+++ src/strings/Demo10.java
@@ -0,0 +1,20 @@
+// To check the capacity of StringBuffer or StringBuilder we need to use capacity()
+// Initial capacity of StringBuffer or StringBuilder is 16
+// If the capacity of filled, then full new capacity would be generated which is always
+// equal to: OLD CAPACITY * 2 + 2
+//                     16 * 2 + 2
+//                    = 34
+
+package strings;
+
+public class Demo10 {
+	public static void main(String[] args) {
+		StringBuffer s1 = new StringBuffer();
+		System.out.println(s1.capacity());
+		s1.append("Virat Kohli is a Batsman");
+		System.out.println(s1.capacity());
+		s1.append("He plays for RCB");
+		System.out.println(s1.capacity());
+	}
+
+}
