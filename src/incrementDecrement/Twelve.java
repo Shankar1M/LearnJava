71e9dc5b0acec8988a50763419971e693c13f201
Shankar1M <shankarmurthy9997@gmail.com>
2025-03-18 22:40:38 +0530

    12 added

---
+++ src/incrementDecrement/Twelve.java
@@ -0,0 +1,12 @@
+package incrementDecrement;
+
+public class Twelve {
+	public static void main(String[] args) {
+		int a =10;
+		int b;
+		b = --a + --a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
