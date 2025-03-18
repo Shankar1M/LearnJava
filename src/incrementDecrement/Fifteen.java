215944189063c9e2f664fb612eb67550571b24aa
Shankar1M <shankarmurthy9997@gmail.com>
2025-03-18 22:46:50 +0530

    fifteen and sixteen added

---
+++ src/incrementDecrement/Fifteen.java
@@ -0,0 +1,13 @@
+package incrementDecrement;
+
+public class Fifteen {
+	public static void main(String[] args) {
+		
+			int a =10;
+			int b;
+			b = a-- + a-- + a++ - ++a; 
+			System.out.println(a);
+			System.out.println(b);
+	}
+
+}
