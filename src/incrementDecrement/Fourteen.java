9c68c8368af37761d897661ecb30840a7e893dc3
Shankar1M <shankarmurthy9997@gmail.com>
2025-03-18 22:43:28 +0530

    14 added

---
+++ src/incrementDecrement/Fourteen.java
@@ -0,0 +1,12 @@
+package incrementDecrement;
+
+public class Fourteen {
+	public static void main(String[] args) {
+			int a =10;
+			int b;
+			b = ++a + ++a + --a;
+			System.out.println(a);
+			System.out.println(b);
+	}
+
+}
